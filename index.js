9bb3078d6b406ddbc015efc8ee4384fcdd2ffda9
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2025-03-10 18:15:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+let topAboutTL = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.topAbout',
+      start: 'top center',
+      end: 'bottom center',
+      scrub: false,
+      markers: false,
+      toggleActions: 'play complete'
+    }
+  })
+
+
+  topAboutTL.from('.topAboutInfo', {opacity: 0, y: 50, duration: 1});
+  topAboutTL.from('#lakeCabin', {opacity: 0, duration: 1});
+  topAboutTL.from('#canoe', {opacity: 0, duration: 1});
+
+  let areaAboutTL = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.areaAbout',
+      start: 'top center',
+      end: 'bottom center',
+      scrub: false,
+      markers: false,
+      toggleActions: 'play complete'
+    }
+  })
+
+  areaAboutTL.from('.areaAboutInfo', {opacity: 0, y: 50, duration: 1});
+  areaAboutTL.from('#bedroom', {opacity: 0, duration: 1});
+  areaAboutTL.from('#bathroom', {opacity: 0, duration: 1});
+  areaAboutTL.from('#masterBed', {opacity: 0, duration: 1});
+
+  let amenitiesTL = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.amenities',
+      start: 'top center',
+      end: 'bottom center',
+      scrub: false,
+      markers: false,
+      toggleActions: 'play complete'
+    }
+  })
+
+  amenitiesTL.from('.amenitiesInfo', {opacity: 0, y: 50, duration: 1});
+  amenitiesTL.from('#boating', {opacity: 0, duration: 1});
+  amenitiesTL.from('#skiing', {opacity: 0, duration: 1});
+  amenitiesTL.from('#fishing', {opacity: 0, duration: 1});
